f5caa2e6a10ebae8b16d5a20ca4523d5e4398c2a
Namredla <aka2493375@gmail.com>
2016-12-01 12:39:02 -0500

    Most recent version. Functioning Toggle Function, and attempts at pulling data from Firebase included.

--- app/src/main/java/firebase/smartplugtestapplication/MainActivity.java
+++ app/src/main/java/firebase/smartplugtestapplication/MainActivity.java
@@ -3,6 +3,8 @@ package firebase.smartplugtestapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -11,40 +13,69 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView powerDisplay;
+    double averageTotalPower = 0;
+    int childCount = 0;
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    DatabaseReference powerRef = database.getReference("PlugData");
-    ChildEventListener newDataListener = new ChildEventListener() {
+        //Test to set testbox value at app startup
+        powerDisplay = (TextView)findViewById(R.id.powerDataDisplay);
+        powerDisplay.setText("Initialized!");
+    }
+    /*powerRef.addChildEventListener(new ChildEventListener(){
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-            
+            childCount++;
+            double childValue = dataSnapshot.getValue(double.class);
+            averageTotalPower = averageTotalPower + ((childValue - averageTotalPower) / childCount);
+            String stringDouble = Double.toString(averageTotalPower);
+            powerDisplay.setText(stringDouble);
         }
+    });*/
+        // Child event listener which gets the value placed in the new child created, and adds it to the average
+        ChildEventListener powerDataListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                childCount++;
+                double childValue = dataSnapshot.getValue(double.class);
+                averageTotalPower = averageTotalPower + ((childValue - averageTotalPower) / childCount);
+                String stringDouble = Double.toString(averageTotalPower);
+                powerDisplay.setText(stringDouble);
+            }
 
-        @Override
-        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-        }
+            }
 
-        @Override
-        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-        }
+            }
 
-        @Override
-        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-        }
+            }
 
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        }
-    };
+            }
+        };
+
+    //Should attach listener described above to the Plug Data reference, but does not work
+    DatabaseReference powerRef = database.getReference("PlugData");
+    powerRef.addChildEventListener(powerDataListener);
+
     public void toggleCommand(View view) {
         DatabaseReference toggleRef = database.getReference("ToggleInput");
         toggleRef.setValue(1);
